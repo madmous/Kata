9557fb1c35dcf2911d64044e705f1aaa033696d6
Moustapha Amadou Diouf <moustapha.diouf@fluo.com>
2018-03-12 08:12:27 +0100

    feat(mars-rover): done

--- MarsRover/src/main.js
+++ MarsRover/src/main.js
@@ -1,5 +1,167 @@
 /** @flow */
 
-export function execute(): void {
-  throw new Error('Not implemented yet');
-}
+/**
+ * DOMAIN
+ */
+export type Game = Array<'r' | 'o' | ' '>[];
+
+export type Direction = 'N' | 'S' | 'E' | 'W';
+
+export type Coordinate = {
+  x: number,
+  y: number,
+};
+
+export type Obstacles = Coordinate[];
+
+export type Dimension = {
+  width: number,
+  height: number,
+};
+
+export type Rover = {
+  coordinate: Coordinate,
+  direction: Direction,
+};
+
+export type World = {
+  rover: Rover,
+  dimension: Dimension,
+  obstacles: Obstacles,
+};
+
+export type MoveCommand = 'f' | 'b';
+export type RotateCommand = 'l' | 'r';
+
+export type Command = MoveCommand | RotateCommand;
+
+/**
+ * WORKFLOW
+ */
+
+export type NextRoverState = (
+  world: World,
+  command: Command
+) => { coordinate: Coordinate } | { direction: Direction };
+
+export type Execute = (world: World, commands: Command[]) => World;
+
+export type MoveRover = (
+  world: World,
+  command: Command
+) => { coordinate: Coordinate };
+
+type NextCoordinateValue = (
+  nextCoordinate: number,
+  dimension: number
+) => number;
+
+type IsRoverOnObstacle = (
+  coordinate: Coordinate,
+  obstacles: Obstacles
+) => boolean;
+
+type RotateRover = (
+  world: World,
+  command: RotateCommand
+) => { direction: Direction };
+
+/**
+ *
+ */
+
+const rotateRover: RotateRover = (world, command) => {
+  const { rover: { direction, coordinate: { x, y } } } = world;
+  const nextDirection = {
+    N: {
+      l: 'W',
+      r: 'E',
+    },
+    S: {
+      l: 'E',
+      r: 'W',
+    },
+    E: {
+      l: 'N',
+      r: 'S',
+    },
+    W: {
+      l: 'S',
+      r: 'N',
+    },
+  }[direction][command];
+
+  return {
+    direction: nextDirection,
+  };
+};
+
+const isRoverOnObstacle: IsRoverOnObstacle = ({ x, y }, obstacles) => {
+  const sameCoordinate = obs => obs.x === x && obs.y === y;
+
+  return obstacles.find(sameCoordinate) !== undefined;
+};
+
+const nextCoordinateValue: NextCoordinateValue = (nexPosition, dimension) =>
+  (nexPosition + dimension) % dimension;
+
+const moveRover: MoveRover = (world, command) => {
+  const {
+    dimension: { width, height },
+    rover: { coordinate, direction },
+    obstacles,
+  } = world;
+  const { x, y } = coordinate;
+  const orientation = command === 'f' ? 1 : -1;
+  const nextCoordinate = {
+    N: () => ({
+      x,
+      y: nextCoordinateValue(y + orientation, height),
+    }),
+    S: () => ({
+      x,
+      y: nextCoordinateValue(y - orientation, height),
+    }),
+    E: () => ({
+      x: nextCoordinateValue(x + orientation, width),
+      y,
+    }),
+    W: () => ({
+      x: nextCoordinateValue(x - orientation, width),
+      y,
+    }),
+  }[direction]();
+
+  if (isRoverOnObstacle(nextCoordinate, obstacles)) {
+    return {
+      coordinate,
+    };
+  }
+
+  return {
+    coordinate: nextCoordinate,
+  };
+};
+
+const nextRoverState: NextRoverState = (world, command) =>
+  ({
+    f: () => moveRover(world, 'f'),
+    b: () => moveRover(world, 'b'),
+    l: () => rotateRover(world, 'l'),
+    r: () => rotateRover(world, 'r'),
+  }[command]());
+
+export const execute: Execute = (world, commands) => {
+  const nextWorldState = commands.reduce(
+    (world, command) => ({
+      ...world,
+      rover: {
+        ...world.rover,
+        ...nextRoverState(world, command),
+      },
+    }),
+    world
+  );
+
+  return nextWorldState;
+};
--- MarsRover/src/main.spec.js
+++ MarsRover/src/main.spec.js
@@ -1,9 +1,163 @@
 /** @flow */
 
 import { execute } from './main';
+import { play } from './game';
+
+import type { Coordinate, Dimension, Obstacles, World, Rover } from './main';
+
+const anObstacleAt33: Coordinate = {
+  x: 3,
+  y: 3,
+};
+const aRoverAt00N: Rover = {
+  coordinate: {
+    x: 0,
+    y: 0,
+  },
+  direction: 'N',
+};
+const a55Dimension: Dimension = {
+  width: 5,
+  height: 5,
+};
+
+describe('Game', () => {
+  xit('should create a 5 by 5 game grid when the world dimension is 5 by 5', async () => {
+    // given
+    const aWorld: World = {
+      rover: aRoverAt00N,
+      dimension: a55Dimension,
+      obstacles: [anObstacleAt33],
+    };
+
+    // when
+    const game = play(aWorld);
+
+    // then
+    expect(game.length).toBe(5);
+  });
+
+  xit('should have a rover at position 0,0 facing north', async () => {
+    // given
+    const aWorld: World = {
+      rover: aRoverAt00N,
+      dimension: a55Dimension,
+      obstacles: [anObstacleAt33],
+    };
+
+    // when
+    const game = play(aWorld);
+
+    // then
+    expect(game[0][0]).toBe('r');
+  });
+
+  xit('should have obstacles at position 3,3 and position 3,4', async () => {
+    // given
+    const aWorld: World = {
+      rover: aRoverAt00N,
+      dimension: a55Dimension,
+      obstacles: [
+        anObstacleAt33,
+        {
+          x: 3,
+          y: 4,
+        },
+      ],
+    };
+
+    // when
+    const game = play(aWorld);
+
+    // then
+    expect(game[3][3]).toBe('o');
+    expect(game[3][4]).toBe('o');
+  });
+});
 
 describe('Rover', () => {
-  it('should move forward when it receives the command "????"', () => {
-    expect(execute).toThrow('Not implemented yet');
+  it('should move freely around the world from one edge to another when it receives commands', () => {
+    // Given
+    const aWorld: World = {
+      rover: aRoverAt00N,
+      dimension: a55Dimension,
+      obstacles: [anObstacleAt33],
+    };
+    const commands = [
+      'l',
+      'f',
+      'b',
+      'l',
+      'f',
+      'b',
+      'l',
+      'b',
+      'f',
+      'r',
+      'b',
+      'r',
+      'b',
+      'f',
+      'f',
+    ];
+    // When
+    const newWorld = execute(aWorld, commands);
+
+    // Then
+    const expectedWorld: World = {
+      rover: {
+        coordinate: {
+          x: 4,
+          y: 1,
+        },
+        direction: 'W',
+      },
+      dimension: {
+        width: 5,
+        height: 5,
+      },
+      obstacles: [anObstacleAt33],
+    };
+    expect(newWorld).toEqual(expectedWorld);
+  });
+
+  it('should not move when an obstacle is detected', () => {
+    // Given
+    const aRoverAt32N: Rover = {
+      ...aRoverAt00N,
+      coordinate: {
+        x: 3,
+        y: 2,
+      },
+    };
+    const obstacles: Obstacles = [
+      anObstacleAt33,
+      {
+        x: 0,
+        y: 2,
+      },
+    ];
+    const aWorld: World = {
+      dimension: a55Dimension,
+      rover: aRoverAt32N,
+      obstacles,
+    };
+    const commands = ['f', 'f', 'r', 'f', 'f'];
+    // When
+    const newWorld = execute(aWorld, commands);
+
+    // Then
+    const expectedWorld: World = {
+      rover: {
+        coordinate: {
+          x: 4,
+          y: 2,
+        },
+        direction: 'E',
+      },
+      dimension: a55Dimension,
+      obstacles,
+    };
+    expect(newWorld).toEqual(expectedWorld);
   });
 });
