6260a870d8425106f445d03cc5c522c9a3b69367
Moustapha Amadou Diouf <moustapha.diouf@fluo.com>
2018-04-30 13:45:26 +0200

    chore(bowling): initial kata setup

---
+++ Bowling/src/main.js
@@ -0,0 +1,8 @@
+/** @flow */
+
+type ScoreRolls = (input: string) => number;
+const scoreRolls: ScoreRolls = input => {
+  throw new Error('Not implemented yet');
+};
+
+export { scoreRolls as default };
