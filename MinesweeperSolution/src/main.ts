4cd4ec277e78d1e455c3345d487ccc01464c7dd6
madmous <mail@madmous.com>
2018-05-05 23:40:35 +0200

    refactor(mine-sweeper): improve naming

--- MinesweeperSolution/src/main.ts
+++ MinesweeperSolution/src/main.ts
@@ -17,11 +17,10 @@ import {
 
 import log from './log';
 
-type FieldInString = string[][];
-type FieldInNumber = number[][];
-type Field = Array<Array<number | string>>;
+type InputField = ReadonlyArray<ReadonlyArray<string>>;
+type Field = number[][];
 
-interface FieldDimension {
+interface FieldBounds {
   width: number;
   height: number;
 }
@@ -36,79 +35,78 @@ interface Position {
   row: number;
 }
 
-type ToString = (matrix: FieldInString) => string;
-const toString: ToString = matrix => flow(map(join('')), join(' '))(matrix);
+type ToString = (field: Field) => string;
+const toString: ToString = field => flow(map(join('')), join(' '))(field);
 
-type FillMines = (
-  matrix: FieldInNumber
-) => (coordinates: Coordinate[]) => FieldInNumber;
-const fillMines: FillMines = matrix => coordinates => {
-  const newMatrix = clone(matrix);
+type AddAdjacentMinesNumberOn = (
+  field: Field
+) => (coordinates: Coordinate[]) => number[];
+const addAdjacentMinesNumberOn: AddAdjacentMinesNumberOn = field => coordinates => {
+  const newField = clone(field);
 
   forEach((coordinate: Coordinate) => {
-    const value = newMatrix[coordinate.x][coordinate.y];
+    const value = newField[coordinate.x][coordinate.y];
 
     if (value !== -1) {
-      newMatrix[coordinate.x][coordinate.y] = add(1)(
-        newMatrix[coordinate.x][coordinate.y]
+      newField[coordinate.x][coordinate.y] = add(1)(
+        newField[coordinate.x][coordinate.y]
       );
     }
   })(coordinates);
 
-  return newMatrix;
+  return flatten(newField);
 };
 
-type RemoveImpossibleValues = (
-  fieldDimension: FieldDimension
+type IsCoordinateOutOf = (
+  filedBounds: FieldBounds
+) => (coordinates: Coordinate) => boolean;
+const isCoordinateOutOf: IsCoordinateOutOf = filedBounds => minePosition =>
+  minePosition.x >= 0 &&
+  minePosition.x < filedBounds.height &&
+  minePosition.y >= 0 &&
+  minePosition.y < filedBounds.width;
+
+type FilterCoordinatesOutOf = (
+  fieldBounds: FieldBounds
 ) => (coordinates: Coordinate[]) => Coordinate[];
-const removeImpossibleValues: RemoveImpossibleValues = fieldDimension => minePositions =>
-  filter(
-    (minePosition: Coordinate) =>
-      minePosition.x >= 0 &&
-      minePosition.x < fieldDimension.height &&
-      minePosition.y >= 0 &&
-      minePosition.y < fieldDimension.width
-  )(minePositions);
+const filterCoordinatesOutOf: FilterCoordinatesOutOf = fieldBounds => minePositions =>
+  filter(isCoordinateOutOf(fieldBounds))(minePositions);
 
-type CalculatePotentialPositionsAroundAMine = (
-  mineCoordinates: Coordinate[]
-) => Coordinate[][];
-const calculatePotentialPositionsAroundAMine: CalculatePotentialPositionsAroundAMine = mineCoordinates => {
-  return map((mineCoordinate: Coordinate) => {
-    const top = { x: mineCoordinate.x, y: add(1)(mineCoordinate.y) };
-    const bottom = { x: mineCoordinate.x, y: subtract(mineCoordinate.y)(1) };
-    const left = { x: subtract(mineCoordinate.x)(1), y: mineCoordinate.y };
-    const right = { x: add(1)(mineCoordinate.x), y: mineCoordinate.y };
+type CalculateAdjacentCoordinateToMine = (
+  coordinate: Coordinate
+) => Coordinate[];
+const calculateAdjacentCoordinateToMine: CalculateAdjacentCoordinateToMine = coordinate => {
+  const { x, y } = coordinate;
+  const top = { x, y: add(1)(y) };
+  const bottom = { x, y: subtract(y)(1) };
+  const left = { x: subtract(x)(1), y };
+  const right = { x: add(1)(x), y };
 
-    const topLeft = {
-      x: subtract(mineCoordinate.x)(1),
-      y: add(1)(mineCoordinate.y),
-    };
-    const topRight = {
-      x: add(1)(mineCoordinate.x),
-      y: add(1)(mineCoordinate.y),
-    };
-    const bottomLeft = {
-      x: subtract(mineCoordinate.x)(1),
-      y: subtract(mineCoordinate.y)(1),
-    };
-    const bottomRight = {
-      x: add(1)(mineCoordinate.x),
-      y: subtract(mineCoordinate.y)(1),
-    };
+  const topLeft = {
+    x: subtract(x)(1),
+    y: add(1)(y),
+  };
+  const topRight = {
+    x: add(1)(x),
+    y: add(1)(y),
+  };
+  const bottomLeft = {
+    x: subtract(x)(1),
+    y: subtract(y)(1),
+  };
+  const bottomRight = {
+    x: add(1)(x),
+    y: subtract(y)(1),
+  };
 
-    return [
-      top,
-      bottom,
-      left,
-      right,
-      topLeft,
-      topRight,
-      bottomLeft,
-      bottomRight,
-    ];
-  })(mineCoordinates);
+  return [top, bottom, left, right, topLeft, topRight, bottomLeft, bottomRight];
 };
+
+type CalculateAdjacentCoordinatesToMine = (
+  coordinates: Coordinate[]
+) => Coordinate[];
+const calculateAdjacentCoordinatesToMine: CalculateAdjacentCoordinatesToMine = coordinates =>
+  flow(map(calculateAdjacentCoordinateToMine), flatten)(coordinates);
 
 type GetNextPosition = (character: string) => (pos: Position) => Position;
 const getNextPosition: GetNextPosition = character => pos => {
@@ -132,24 +130,25 @@ const getNextCoordinates: GetMineCoordinates = coordinates => character => pos =
 };
 
 type FindMineCoordinates = (
-  coordinates: Coordinate[]
-) => (pos: Position) => (input: string) => Coordinate[];
-const findMineCoordinates: FindMineCoordinates = coordinates => pos => input => {
+  accumulutator: Coordinate[]
+) => (position: Position) => (input: string) => Coordinate[];
+const findMineCoordinates: FindMineCoordinates = accumulutator => position => input => {
   if (input.length === 0) {
-    return coordinates;
+    return accumulutator;
   } else {
     const firstChar = head(input);
-
-    const nextCoordinates = getNextCoordinates(coordinates)(firstChar)(pos);
-    const nextPos = getNextPosition(firstChar)(pos);
+    const nextCoordinates = getNextCoordinates(accumulutator)(firstChar)(
+      position
+    );
+    const nextPos = getNextPosition(firstChar)(position);
     const nextInput = input.substring(1);
 
     return findMineCoordinates(nextCoordinates)(nextPos)(nextInput);
   }
 };
 
-type PlaceMinesIfNecessary = (field: number) => number | string;
-const placeMinesIfNecessary: PlaceMinesIfNecessary = value => {
+type placeMineCharacterIfNcessaryOn = (field: number) => number | string;
+const placeMineCharacterIfNcessaryOn: placeMineCharacterIfNcessaryOn = value => {
   if (value === -1) {
     return '*';
   } else {
@@ -157,20 +156,21 @@ const placeMinesIfNecessary: PlaceMinesIfNecessary = value => {
   }
 };
 
-type PlaceMines = (field: number[]) => Array<number | string>;
-const placeMines: PlaceMines = field => map(placeMinesIfNecessary)(field);
+type placeMineCharacterOnField = (field: number[]) => Array<number | string>;
+const placeMineCharacterOnField: placeMineCharacterOnField = field =>
+  map(placeMineCharacterIfNcessaryOn)(field);
 
-type RestoreField = (
+type ToField = (
   width: number
-) => (matrix: FieldInNumber) => (fllatennedMatrix: number[]) => FieldInNumber;
-const restoreField: RestoreField = width => matrix => fllatennedMatrix => {
+) => (accumulator: Field) => (fllatennedMatrix: number[]) => Field;
+const toField: ToField = width => accumulator => fllatennedMatrix => {
   if (fllatennedMatrix.length === 0) {
-    return matrix;
+    return accumulator;
   } else {
-    const nextMatrix = concat(matrix)([take(width)(fllatennedMatrix)]);
+    const nextMatrix = concat(accumulator)([take(width)(fllatennedMatrix)]);
     const nextFllatennedMatrix = drop(width)(fllatennedMatrix);
 
-    return restoreField(width)(nextMatrix)(nextFllatennedMatrix);
+    return toField(width)(nextMatrix)(nextFllatennedMatrix);
   }
 };
 
@@ -187,35 +187,33 @@ type ReplaceDotsWithZeros = (input: string[]) => number[];
 const replaceDotsWithZeros: ReplaceDotsWithZeros = input =>
   map(replaceDotsWithZerosIfNecessary)(input);
 
-type GetFieldDimension = (matrix: FieldInString) => FieldDimension;
-const getFieldDimension: GetFieldDimension = matrix => ({
-  height: matrix.length,
-  width: head(matrix).length,
+type GetFieldBounds = (inputField: InputField) => FieldBounds;
+const getFieldBounds: GetFieldBounds = inputField => ({
+  height: inputField.length,
+  width: head(inputField).length,
 });
 
-type ToMatrix = (input: string) => FieldInString;
-const toMatrix: ToMatrix = input => flow(split(' '), map(split('')))(input);
+type ToInputField = (input: string) => InputField;
+const toInputField: ToInputField = input => flow(split(' '), map(split('')))(input);
 
 type FindMines = (input: string) => string;
 const findMines: FindMines = input => {
-  const matrix = toMatrix(input);
-  const fieldDimension = getFieldDimension(matrix);
+  const inputField = toInputField(input);
+  const fieldBounds = getFieldBounds(inputField);
 
-  const filledMatrix = flow(
+  const field = flow(
     flatten,
     replaceDotsWithZeros,
-    restoreField(fieldDimension.width)([])
-  )(matrix);
+    toField(fieldBounds.width)([])
+  )(inputField);
 
   return flow(
     findMineCoordinates([])({ row: 0, column: 0 }),
-    calculatePotentialPositionsAroundAMine,
-    flatten,
-    removeImpossibleValues(fieldDimension),
-    fillMines(filledMatrix),
-    flatten,
-    placeMines,
-    restoreField(fieldDimension.width)([]),
+    calculateAdjacentCoordinatesToMine,
+    filterCoordinatesOutOf(fieldBounds),
+    addAdjacentMinesNumberOn(field),
+    placeMineCharacterOnField,
+    toField(fieldBounds.width)([]),
     toString
   )(input);
 };
