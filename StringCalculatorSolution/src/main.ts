19d19e269fdb52c8cd0da97a426604c6348a5bef
madmous <mail@madmous.com>
2018-05-12 00:30:05 +0200

    refactor(string-calculator-solution): better naming and document structure

--- StringCalculatorSolution/src/main.ts
+++ StringCalculatorSolution/src/main.ts
@@ -15,8 +15,9 @@ import {
 
 import log from './log';
 
-type NewLineDelimiter = '\n';
 type CommaSeparator = ',';
+type NewLineDelimiter = '\n';
+type PrefixDelimiter = '//';
 
 type ReplaceIf = <T>(predicate: (value: T) => boolean) => (defaultValue: T) => (value: T) => T;
 const replaceIf: ReplaceIf = predicate => defaultValue => value => {
@@ -27,80 +28,55 @@ const replaceIf: ReplaceIf = predicate => defaultValue => value => {
   }
 };
 
-type CheckForNegativeNumbers = (numbers: number[]) => number[];
-const checkForNegativeNumbers: CheckForNegativeNumbers = numbers => {
-  const toResult = (negativeNumberIndex: number) => {
-    if (negativeNumberIndex === -1) {
-      return numbers;
-    } else {
-      throw new Error('negatives not allowed');
-    }
-  };
-  const isNegative = (num: number) => num < 0;
-
-  return flow(findIndex(isNegative), toResult)(numbers);
-};
-
-type ReplaceNewLineDelimiterWith = (
-  commaSeparator: CommaSeparator
-) => (delimiter: NewLineDelimiter) => (string: string) => string;
-const replaceNewLineDelimiterWith: ReplaceNewLineDelimiterWith = commaSeparator => delimiter => string => {
-  const isNotADelimiter = (char: string) => char !== delimiter;
-
-  return flow(split(''), map(replaceIf(isNotADelimiter)(commaSeparator)), join(''))(string);
-};
-
-type FilterWith = (commaSeparator: CommaSeparator) => (strings: string[]) => string[];
-const filterWith: FilterWith = commaSeparator => (strings: string[]) =>
-  flow(join(''), split(commaSeparator))(strings);
-
-type IsDelimiterIn = (string: string) => (character: string) => boolean;
-const isDelimiterIn: IsDelimiterIn = string => character => indexOf(character)(string) === -1;
-
-type ReplaceDelimitersThenCollect = (
-  commaSeparator: CommaSeparator
-) => (
-  newLineDelimiter: NewLineDelimiter
-) => (prefixDelimiter: string) => (character: string) => string[];
-const replaceDelimitersThenCollect: ReplaceDelimitersThenCollect = commaSeparator => newLineDelimiter => prefixDelimiter => character => {
-  const filterWith = (commaSeparator: CommaSeparator) => (strings: string[]) =>
-    flow(join(''), split(commaSeparator))(strings);
-
-  const [head, numbers] = split(newLineDelimiter)(character);
-  const [_, delimiter] = split(prefixDelimiter)(head);
-  const removedBrackets = join('')(
-    filter((strings: string) => strings !== '[' || strings !== '[')(delimiter)
-  );
-
-  return flow(
-    map(replaceIf(isDelimiterIn(removedBrackets))(commaSeparator)),
-    filterWith(commaSeparator)
-  )(numbers);
-};
-
-type CollectNumbersToAdd = (
-  commaSeparator: CommaSeparator
-) => (
-  newLineDelimiter: NewLineDelimiter
-) => (prefixDelimiter: string) => (character: string) => string[];
-const collectNumbersToAdd: CollectNumbersToAdd = commaSeparator => newLineDelimiter => prefixDelimiter => character => {
-  if (startsWith(prefixDelimiter)(character)) {
-    return replaceDelimitersThenCollect(commaSeparator)(newLineDelimiter)(prefixDelimiter)(
-      character
-    );
-  } else {
-    return flow(
-      replaceNewLineDelimiterWith(commaSeparator)(newLineDelimiter),
-      split(commaSeparator)
-    )(character);
-  }
-};
-
 type IsBelow = (threshold: number) => (value: number) => boolean;
 const isBelow: IsBelow = threshold => value => value < threshold;
 
-type ToInt = (character: string) => number;
-const toInt: ToInt = character => parseInt(character, 10);
+type ThrowIfNegativeNumbers = (numbers: number[]) => number[];
+const throwIfNegativeNumbers: ThrowIfNegativeNumbers = numbers => {
+  const negativeNumberIndex = findIndex(isBelow(0))(numbers);
+
+  if (negativeNumberIndex === -1) {
+    return numbers;
+  } else {
+    throw new Error('negatives not allowed');
+  }
+};
+
+type ToNumber = (numberInString: string) => number;
+const toNumber: ToNumber = numberInString => parseInt(numberInString, 10);
+
+type RemoveOccurencesOf = (
+  commaSeparator: CommaSeparator
+) => (numbersInString: string[]) => string[];
+const removeOccurencesOf: RemoveOccurencesOf = commaSeparator => numbersInString =>
+  flow(join(''), split(commaSeparator))(numbersInString);
+
+type IsNot = (delimiter: string) => (char: string) => boolean;
+const isNot: IsNot = delimiter => char => char !== delimiter;
+
+type IsPresentIn = (delimiter: string) => (numberInString: string) => boolean;
+const isPresentIn: IsPresentIn = (delimiter: string) => (numberInString: string) =>
+  indexOf(numberInString)(delimiter) === -1;
+
+type IsBracket = (strings: string) => boolean;
+const isBracket: IsBracket = strings => strings !== '[' || strings !== '[';
+
+type ReplaceDelimitersIfNecessaryWith = (
+  commaSeparator: CommaSeparator
+) => (
+  newLineDelimiter: NewLineDelimiter
+) => (prefixDelimiter: PrefixDelimiter) => (input: string) => string[];
+const replaceDelimitersIfNecessaryWith: ReplaceDelimitersIfNecessaryWith = commaSeparator => newLineDelimiter => prefixDelimiter => input => {
+  if (startsWith(prefixDelimiter)(input)) {
+    const [head, numbersInString] = split(newLineDelimiter)(input);
+    const [_, maybeDelimiterWithBrackets] = split(prefixDelimiter)(head);
+    const delimiter = join('')(filter(isBracket)(maybeDelimiterWithBrackets));
+
+    return map(replaceIf(isPresentIn(delimiter))(commaSeparator))(numbersInString);
+  } else {
+    return map(replaceIf(isNot(newLineDelimiter))(commaSeparator))(input);
+  }
+};
 
 type AddNumbers = (input: string) => number;
 const addNumbers: AddNumbers = input => {
@@ -110,10 +86,11 @@ const addNumbers: AddNumbers = input => {
   const PREFIX_DELIMITER = '//';
 
   return flow(
-    collectNumbersToAdd(COMMA_SEPARATOR)(NEW_LINE_DELIMITER)(PREFIX_DELIMITER),
-    map(toInt),
+    replaceDelimitersIfNecessaryWith(COMMA_SEPARATOR)(NEW_LINE_DELIMITER)(PREFIX_DELIMITER),
+    removeOccurencesOf(COMMA_SEPARATOR),
+    map(toNumber),
     filter(isBelow(BIG_NUMBERS)),
-    checkForNegativeNumbers,
+    throwIfNegativeNumbers,
     sum
   )(input);
 };
