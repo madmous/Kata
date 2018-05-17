a438f5aa6771fe3e6c48a2c2b2f25ee62e9cbc56
Moustapha Amadou Diouf <moustapha.diouf@fluo.com>
2018-05-17 14:24:10 +0200

    feat(organize-imports-solution): add native, local and type modules cases

--- OrganizeImports/src/main.js
+++ OrganizeImports/src/main.js
@@ -1,26 +1,98 @@
 /** @flow */
-import { flow, join, last, map, split } from 'lodash/fp';
+import {
+  filter,
+  flow,
+  head,
+  includes,
+  join,
+  last,
+  split,
+  startsWith,
+  tail,
+} from 'lodash/fp';
 
 // import log from './log';
 
-type importsByModule = { name: string, import: string };
+type ModulesType = 'nativeModules' | 'typeModules' | 'localModules';
 
-type GroupImportsByModule = (strings: string[]) => importsByModule[];
-const groupImportsByModule: GroupImportsByModule = strings => {
-  return map(v => {
-    const module = last(split(' ')(v));
+type ModuleType = 'nativeModule' | 'typeModule' | 'localModule';
 
-    return {
-      name: module,
-      import: v,
-    };
-  })(strings);
+type Import = { name: string, description: string, type: ModuleType };
+
+type ImportsByModuleType = { [key: ModulesType]: Import[] };
+
+type IsLocalModule = (string: string) => boolean;
+const isLocalModule: IsLocalModule = string =>
+  startsWith("'/")(string) || startsWith("'..")(string);
+
+type IsTypeModule = (string: string) => boolean;
+const isTypeModule: IsTypeModule = string => includes('type')(string);
+
+type GetImportType = (
+  description: string
+) => (description: string) => ModuleType;
+const getImportType: GetImportType = description => name => {
+  if (isTypeModule(description)) {
+    return 'typeModule';
+  } else if (isLocalModule(name)) {
+    return 'localModule';
+  } else {
+    return 'nativeModule';
+  }
 };
 
-type OrderAlphabetically = (
-  a: importsByModule,
-  b: importsByModule
-) => 1 | -1 | 0;
+type GroupImportByModule = (strings: string) => Import;
+const groupImportByModule: GroupImportByModule = string => {
+  const module = last(split(' ')(string));
+  const type = getImportType(string)(module);
+
+  return {
+    name: module,
+    description: string,
+    type,
+  };
+};
+
+type ToNextBuildOutputAcc = (acc: string[]) => (imports: Import[]) => string[];
+const toNextBuildOutputAcc: ToNextBuildOutputAcc = acc => imports => {
+  const headImport = head(imports);
+  if (typeof headImport === 'string') {
+    return [...acc, ''];
+  } else {
+    const { description } = head(imports);
+
+    return [...acc, description];
+  }
+};
+
+type BuildOutput = (acc: string[]) => (imports: Import[]) => string[];
+const buildOutput: BuildOutput = acc => imports => {
+  if (imports.length === 0) {
+    return acc;
+  } else {
+    const nextAcc = toNextBuildOutputAcc(acc)(imports);
+    const nextImports = tail(imports);
+
+    return buildOutput(nextAcc)(nextImports);
+  }
+};
+
+type ConcatModules = (
+  importsByModuleType: ImportsByModuleType
+) => Array<Import | ''>;
+const concatModules: ConcatModules = importsByModuleType => {
+  const { localModules, nativeModules, typeModules } = importsByModuleType;
+
+  if (nativeModules.length > 0 && typeModules.length > 0 && localModules.length > 0) {
+    return [...nativeModules, '', ...localModules ,'', ...typeModules];
+  } else if (nativeModules.length > 0 && typeModules.length > 0) {
+    return [...nativeModules, '', ...typeModules];
+  } else {
+    return [...nativeModules];
+  }
+};
+
+type OrderAlphabetically = (a: Import, b: Import) => 1 | -1 | 0;
 const orderAlphabetically: OrderAlphabetically = (a, b) => {
   if (a.name < b.name) {
     return -1;
@@ -31,23 +103,83 @@ const orderAlphabetically: OrderAlphabetically = (a, b) => {
   }
 };
 
-type Sort = (
-  fn: (importsByModule, importsByModule) => 1 | -1 | 0
-) => (importsByModule[]) => importsByModule[];
+type Sort = (fn: (Import, Import) => 1 | -1 | 0) => (Import[]) => Import[];
 const sort: Sort = fn => importsByModule =>
   Array.from(importsByModule, x => x).sort(fn);
 
+type SortImportsByModuleType = (
+  importsByModuleType: ImportsByModuleType
+) => ImportsByModuleType;
+const sortImportsByModuleType: SortImportsByModuleType = importsByModuleType => {
+  const { typeModules, localModules, nativeModules } = importsByModuleType;
+
+  const orderedTypeModules = sort(orderAlphabetically)(typeModules);
+  const orderedNativeModules = sort(orderAlphabetically)(nativeModules);
+  const orderedLocalModules = sort(orderAlphabetically)(localModules);
+
+  return {
+    typeModules: orderedTypeModules,
+    nativeModules: orderedNativeModules,
+    localModules: orderedLocalModules,
+  };
+};
+
+type ToNextGroupImportsByModuleTypeAcc = (
+  acc: ImportsByModuleType
+) => (string: string) => ImportsByModuleType;
+const toNextGroupImportsByModuleTypeAcc: ToNextGroupImportsByModuleTypeAcc = acc => string => {
+  const importByModule = groupImportByModule(string);
+
+  switch (importByModule.type) {
+    case 'typeModule':
+      return {
+        ...acc,
+        typeModules: [...acc.typeModules, importByModule],
+      };
+    case 'localModule':
+      return {
+        ...acc,
+        localModules: [...acc.localModules, importByModule],
+      };
+    default:
+      return {
+        ...acc,
+        nativeModules: [...acc.nativeModules, importByModule],
+      };
+  }
+};
+
+type GroupImportsByModuleType = (
+  acc: ImportsByModuleType
+) => (strings: string[]) => ImportsByModuleType;
+const groupImportsByModuleType: GroupImportsByModuleType = acc => strings => {
+  if (strings.length === 0) {
+    return acc;
+  } else {
+    const nextAcc = toNextGroupImportsByModuleTypeAcc(acc)(head(strings));
+    const nextStrings = tail(strings);
+
+    return groupImportsByModuleType(nextAcc)(nextStrings);
+  }
+};
+
 type OrganizeImports = (input: string) => string;
 const organizeImports: OrganizeImports = input => {
-  const organizedImports = flow(
-    split('\n'),
-    groupImportsByModule,
-    sort(orderAlphabetically),
-    map(v => v.import),
-    join('\n')
-  )(input);
+  const RENTURN_SYMBOL = '\n';
 
-  return organizedImports;
+  return flow(
+    split(RENTURN_SYMBOL),
+    filter(v => v !== ''),
+    groupImportsByModuleType({
+      typeModules: [],
+      nativeModules: [],
+      localModules: [],
+    }),
+    sortImportsByModuleType,
+    concatModules,
+    buildOutput([]),
+    join(RENTURN_SYMBOL)
+  )(input);
 };
 
 export { organizeImports as default };
--- OrganizeImports/src/main.spec.js
+++ OrganizeImports/src/main.spec.js
@@ -14,20 +14,7 @@ describe('Organize imports', () => {
     expect(organizedImports).toEqual(`import KoaRouter from 'koa-router';`);
   });
 
-  it('should sort imports when there are 2 imports', () => {
-    //given
-    const input = `import logger from 'winston';
-import KoaRouter from 'koa-router';`;
-
-    //when
-    const organizedImports = organizeImports(input);
-
-    //then
-    expect(organizedImports).toEqual(`import KoaRouter from 'koa-router';
-import logger from 'winston';`);
-  });
-
-  it('should sort imports when there are more than 2 imports', () => {
+  it('should sort imports when there are only native imports', () => {
     //given
     const input = `import logger from 'winston';
 import KoaRouter from 'koa-router';
@@ -40,5 +27,49 @@ import uuid from 'uuid';`;
     expect(organizedImports).toEqual(`import KoaRouter from 'koa-router';
 import uuid from 'uuid';
 import logger from 'winston';`);
+  });
+
+  it('should sort imports when there are native and type modules', () => {
+    //given
+    const input = `import logger from 'winston';
+import KoaRouter from 'koa-router';
+import uuid from 'uuid';
+
+import type { Car } from '../cars';`;
+
+    //when
+    const organizedImports = organizeImports(input);
+
+    //then
+    expect(organizedImports).toEqual(`import KoaRouter from 'koa-router';
+import uuid from 'uuid';
+import logger from 'winston';
+
+import type { Car } from '../cars';`);
+  });
+
+  it('should sort imports when there are native, local and type modules', () => {
+    //given
+    const input = `import logger from 'winston';
+import KoaRouter from 'koa-router';
+import uuid from 'uuid';
+
+import { contactInfo } from '../contract';
+import { carInfo } from '../cars';
+
+import type { Car } from '../cars';`;
+
+    //when
+    const organizedImports = organizeImports(input);
+
+    //then
+    expect(organizedImports).toEqual(`import KoaRouter from 'koa-router';
+import uuid from 'uuid';
+import logger from 'winston';
+
+import { carInfo } from '../cars';
+import { contactInfo } from '../contract';
+
+import type { Car } from '../cars';`);
   });
 });
