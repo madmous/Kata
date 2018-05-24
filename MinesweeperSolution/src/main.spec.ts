88974a2ae558d12d1bc2cdb300cb9bdd7b413b1e
Moustapha Amadou Diouf <moustapha.diouf@fluo.com>
2018-05-24 13:15:39 +0200

    refactor(minesweeper-solution): organize document

--- MinesweeperSolution/src/main.spec.ts
+++ MinesweeperSolution/src/main.spec.ts
@@ -5,7 +5,7 @@ describe('Find mines', () => {
     { field: '.', result: '0' },
     { field: '... ...', result: '000 000' },
   ].forEach(({ field, result }) => {
-    it(`should not find any adjacent cell to a mine when the field has no mines`, () => {
+    it(`should not find any adjacent cell to a mine when the field has no mines ${field}`, () => {
     // when
     const mines = findMines(field);
 
@@ -18,7 +18,7 @@ describe('Find mines', () => {
     { field: '*', result: '*' },
     { field: '*** ***', result: '*** ***' },
   ].forEach(({ field, result }) => {
-    it(`should not find adjacent cell to a mine when the field is full of mines`, () => {
+    it(`should not find adjacent cell to a mine when the field is full of mines ${field}`, () => {
     // when
     const mines = findMines(field);
 
@@ -29,35 +29,16 @@ describe('Find mines', () => {
 
   [
     { field: '*.', result: '*1' },
+    { field: '*... ....', result: '*100 1100' },
+    { field: '*... .... .*.. ....', result: '*100 2210 1*10 1110' },
+    { field: '**... ..... .*...', result: '**100 33200 1*100' },
   ].forEach(({ field, result }) => {
-    it(`should find adjacent cell to a mine when the field has mines`, () => {
+    it(`should correctly count adjacent cells to a mine when the field has mines ${field}`, () => {
     // when
     const mines = findMines(field);
 
     // then
     expect(mines).toEqual(result);
     });
-  });
-
-  xit('should not find any mine when the field has no mines', () => {
-    // given
-    const field = `*... .... .*.. ....`;
-
-    // when
-    const mines = findMines(field);
-
-    // then
-    expect(mines).toEqual('*100 2210 1*10 1110');
-  });
-
-  xit('should not find any mine when the field has no mines', () => {
-    // given
-    const field = '**... ..... .*...';
-
-    // when
-    const mines = findMines(field);
-
-    // then
-    expect(mines).toEqual('**100 33200 1*100');
   });
 });
--- MinesweeperSolution/src/main.ts
+++ MinesweeperSolution/src/main.ts
@@ -1,73 +1,242 @@
 import {
+  add,
   concat,
+  differenceWith,
   drop,
-  head,
-  join,
+  filter,
   flow,
+  head,
+  isEqual,
+  join,
   map,
-  nth,
   reduce,
+  split,
+  subtract,
   tail,
+  take,
 } from 'lodash/fp';
+
+import { adjust, update } from 'ramda';
+
+import { flatten } from './utils';
 
 import log from './log';
 
+export type InputField = ReadonlyArray<ReadonlyArray<string>>;
+export type Field = ReadonlyArray<ReadonlyArray<number>>;
 
-
-
-type GetNextCurrentCells = (currentCell: string) => (rightCell: string) => (cells: string[]) => string[];
-const getNextCurrentCells: GetNextCurrentCells = currentCell => rightCell => cells => {
-  if (currentCell === '*') {
-    if (rightCell !== undefined && rightCell !== '*' && rightCell !== ' ') {
-      return drop(2)(cells);
-    } else {
-      return drop(1)(cells);
-    }
-  } else {
-    return drop(1)(cells);
-  }
+interface FieldBounds {
+  width: number;
+  height: number;
 }
 
-type GetNextAcc = (currentCell: string) => (rightCell: string) => (acc: string[]) => string[];
-const getNextAcc: GetNextAcc = currentCell => rightCell => acc => {
-  if (currentCell === '*') {
-    if (rightCell !== undefined && rightCell !== '*' && rightCell !== ' ') {
-      return [...acc, '*', '1'];
-    } else {
-      return [...acc, currentCell];
-    }
-  } else {
-    return [...acc, currentCell];
-  }
+export interface Coordinate {
+  x: number;
+  y: number;
 }
 
-type Count = (acc: string[]) => (currentCells: string[]) => string[];
-const count: Count = acc => currentCells => {
-  if (currentCells.length === 0) {
-    return acc;
-  } else {
-    const cell = head(currentCells);
-    const rightCell = nth(1)(currentCells);
-
-    const nextAcc = getNextAcc(cell)(rightCell)(acc);
-    const nextCurrentCells = getNextCurrentCells(cell)(rightCell)(currentCells);
-
-    return count(nextAcc)(nextCurrentCells);
-  }
-};
-
-type ToCell = (inputCell: string) => string;
-const toCell: ToCell = inputCell => {
-  if (inputCell === '.') {
-    return '0';
-  } else {
-    return inputCell;
-  }
-};
+interface Position {
+  column: number;
+  row: number;
+}
 
 type FindMines = (input: string) => string;
 const findMines: FindMines = input => {
-  return flow(map(toCell), count([]), join(''))(input);
+  const inputField = toInputField(input);
+  const fieldBounds = getFieldBounds(inputField);
+
+  const field = flow(replaceDotsWithZeros, toField(fieldBounds.width)([]))(
+    inputField
+  );
+
+  const mines = findMineCoordinates([])({ row: 0, column: 0 })(input);
+
+  return flow(
+    calculateAdjacentCoordinatesToMine,
+    filterCoordinatesOutOf(fieldBounds),
+    filterCoordinatesEqualTo(mines),
+    countAdjacentMinesOn(field),
+    placeMineCharacterOnField,
+    toField(fieldBounds.width)([]),
+    toString
+  )(mines);
 };
 
 export { findMines as default };
+
+type ToInputField = (input: string) => InputField;
+const toInputField: ToInputField = input =>
+  flow(split(' '), map(split('')))(input);
+
+type GetFieldBounds = (inputField: InputField) => FieldBounds;
+const getFieldBounds: GetFieldBounds = inputField => ({
+  height: inputField.length,
+  width: head(inputField).length,
+});
+
+type ReplaceDotsWithZeros = (inputField: InputField) => ReadonlyArray<number>;
+const replaceDotsWithZeros: ReplaceDotsWithZeros = inputField =>
+  map(replaceDotsWithZerosIfNecessary)(flatten(inputField));
+
+type ReplaceDotsWithZerosIfNecessary = (input: string) => number;
+const replaceDotsWithZerosIfNecessary: ReplaceDotsWithZerosIfNecessary = input => {
+  if (input === '.') {
+    return 0;
+  } else {
+    return -1;
+  }
+};
+
+type ToField = (
+  width: number
+) => (accumulator: Field) => (fllatennedField: ReadonlyArray<number>) => Field;
+const toField: ToField = width => accumulator => fllatennedField => {
+  const acc = [...accumulator];
+  if (fllatennedField.length === 0) {
+    return accumulator;
+  } else {
+    const nextField = concat(acc)([take(width)(fllatennedField)]);
+    const nextFllatennedField = drop(width)(fllatennedField);
+
+    return toField(width)(nextField)(nextFllatennedField);
+  }
+};
+
+type FindMineCoordinates = (
+  accumulutator: ReadonlyArray<Coordinate>
+) => (position: Position) => (input: string) => ReadonlyArray<Coordinate>;
+const findMineCoordinates: FindMineCoordinates = accumulutator => position => input => {
+  if (input.length === 0) {
+    return accumulutator;
+  } else {
+    const firstChar = head(input);
+    const nextCoordinates = getNextCoordinates(accumulutator)(firstChar)(
+      position
+    );
+    const nextPos = getNextPosition(firstChar)(position);
+    const nextInput = input.substring(1);
+
+    return findMineCoordinates(nextCoordinates)(nextPos)(nextInput);
+  }
+};
+
+type GetNextCoordinates = (
+  coordinates: ReadonlyArray<Coordinate>
+) => (character: string) => (position: Position) => ReadonlyArray<Coordinate>;
+const getNextCoordinates: GetNextCoordinates = coordinates => character => position => {
+  const { row, column } = position;
+  if (character === '*') {
+    return [...coordinates, { x: row, y: column }];
+  } else {
+    return coordinates;
+  }
+};
+
+type CalculateAdjacentCoordinateToMine = (
+  coordinate: Coordinate
+) => ReadonlyArray<Coordinate>;
+const calculateAdjacentCoordinateToMine: CalculateAdjacentCoordinateToMine = coordinate => {
+  const { x, y } = coordinate;
+  const top = { x, y: add(1)(y) };
+  const bottom = { x, y: subtract(y)(1) };
+  const left = { x: subtract(x)(1), y };
+  const right = { x: add(1)(x), y };
+
+  const topLeft = {
+    x: subtract(x)(1),
+    y: add(1)(y),
+  };
+  const topRight = {
+    x: add(1)(x),
+    y: add(1)(y),
+  };
+  const bottomLeft = {
+    x: subtract(x)(1),
+    y: subtract(y)(1),
+  };
+  const bottomRight = {
+    x: add(1)(x),
+    y: subtract(y)(1),
+  };
+
+  return [top, bottom, left, right, topLeft, topRight, bottomLeft, bottomRight];
+};
+
+type CalculateAdjacentCoordinatesToMine = (
+  coordinates: ReadonlyArray<Coordinate>
+) => ReadonlyArray<Coordinate>;
+const calculateAdjacentCoordinatesToMine: CalculateAdjacentCoordinatesToMine = coordinates =>
+  flow(map(calculateAdjacentCoordinateToMine), flatten)(coordinates);
+
+type GetNextPosition = (character: string) => (position: Position) => Position;
+const getNextPosition: GetNextPosition = character => position => {
+  const { row, column } = position;
+  if (character === ' ') {
+    return { column: 0, row: add(1)(row) };
+  } else {
+    return { column: add(1)(column), row };
+  }
+};
+
+type FilterCoordinatesOutOf = (
+  fieldBounds: FieldBounds
+) => (coordinates: ReadonlyArray<Coordinate>) => ReadonlyArray<Coordinate>;
+const filterCoordinatesOutOf: FilterCoordinatesOutOf = fieldBounds => minePositions =>
+  filter(isCoordinateOutOf(fieldBounds))(minePositions);
+
+type IsCoordinateOutOf = (
+  filedBounds: FieldBounds
+) => (coordinates: Coordinate) => boolean;
+const isCoordinateOutOf: IsCoordinateOutOf = filedBounds => minePosition =>
+  minePosition.x >= 0 &&
+  minePosition.x < filedBounds.height &&
+  minePosition.y >= 0 &&
+  minePosition.y < filedBounds.width;
+
+type FilterCoordinatesEqualTo = (
+  mines: ReadonlyArray<Coordinate>
+) => (adjacentSquares: ReadonlyArray<Coordinate>) => ReadonlyArray<Coordinate>;
+const filterCoordinatesEqualTo: FilterCoordinatesEqualTo = mines => adjacentSquares => {
+  return differenceWith(isEqual, adjacentSquares, mines);
+};
+
+type CountAdjacentMinesOn = (
+  field: Field
+) => (coordinates: ReadonlyArray<Coordinate>) => ReadonlyArray<number>;
+const countAdjacentMinesOn: CountAdjacentMinesOn = field => coordinates => {
+  if (coordinates.length === 0) {
+    return flatten(field);
+  } else {
+    const coordinate = head(coordinates);
+    const nextCoordinates = tail(coordinates);
+    const nextField = toNextField(field)(coordinate);
+
+    return countAdjacentMinesOn(nextField)(nextCoordinates);
+  }
+};
+
+type ToNextField = (field: Field) => (coordinate: Coordinate) => Field;
+const toNextField: ToNextField = field => coordinate => {
+  const { x, y } = coordinate;
+  const nextRow = adjust(add(1), y, field[x]);
+
+  return update(x, nextRow, field);
+};
+
+type placeMineCharacterOnField = (
+  field: ReadonlyArray<number>
+) => ReadonlyArray<number | string>;
+const placeMineCharacterOnField: placeMineCharacterOnField = field =>
+  map(placeMineCharacterIfNcessaryOn)(field);
+
+type ToString = (field: Field) => string;
+const toString: ToString = field => flow(map(join('')), join(' '))(field);
+
+type placeMineCharacterIfNcessaryOn = (field: number) => number | string;
+const placeMineCharacterIfNcessaryOn: placeMineCharacterIfNcessaryOn = value => {
+  if (value === -1) {
+    return '*';
+  } else {
+    return value;
+  }
+};
