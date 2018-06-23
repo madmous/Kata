aa3b6b3326cacf0b280b816f8b2b0d94503465b9
madmous <mail@madmous.com>
2018-06-24 01:09:02 +0200

    chore(mine-sweeper): add readme symbolic links

--- Minesweeper/src/main.js
+++
@@ -1,8 +0,0 @@
-/** @flow */
-
-type FindMines = (input: string) => string;
-const findMines: FindMines = input => {
-  throw new Error('Not implemented yet');
-};
-
-export { findMines as default };
