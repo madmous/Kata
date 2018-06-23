7d026a3402903b00e09e74a98ec0ee42fca6e34f
madmous <mail@madmous.com>
2018-06-24 01:28:33 +0200

    chore(bank): change language prefix

--- Bank/src/main.js
+++
@@ -1,9 +0,0 @@
-/** @flow */
-
-type Execute = () => void;
-
-const execute: Execute = () => {
-  throw new Error('Not implemented yet');
-};
-
-export { execute };
--- Bank/src/main.spec.js
+++
@@ -1,9 +0,0 @@
-/** @flow */
-
-import { execute } from './main';
-
-describe('Test', () => {
-  it('should throw', () => {
-    expect(() => execute()).toThrow('Not implemented yet');
-  });
-});
