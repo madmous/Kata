80a41f5214e15854f3f9bd9e6f1a29f0957c09c7
madmous <mail@madmous.com>
2018-06-24 00:38:50 +0200

    chore(katas): reorganize folders

--- Bowling/src/main.js
+++
@@ -1,8 +0,0 @@
-/** @flow */
-
-type ScoreRolls = (input: string) => number;
-const scoreRolls: ScoreRolls = input => {
-  throw new Error('Not implemented yet');
-};
-
-export { scoreRolls as default };
