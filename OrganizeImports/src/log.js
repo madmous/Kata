f4ccb71734c703b652bf5df4fb2beb0ad1a176f9
madmous <mail@madmous.com>
2018-06-24 01:10:37 +0200

    chore(katas): move out projects

--- OrganizeImports/src/log.js
+++
@@ -1,9 +0,0 @@
-// @flow
-
-type Log<A> = (nbr: number) => (val: A) => A;
-const log: Log<*> = nbr => val => {
-  console.log(`log ${nbr}: `, val);
-  return val;
-};
-
-export { log as default };
--- OrganizeImports/src/main.js
+++
@@ -1,8 +0,0 @@
-/** @flow */
-
-type OrganizeImports = (input: string) => string;
-const organizeImports: OrganizeImports = input => {
-  throw new Error('not implemented yet');
-};
-
-export { organizeImports as default };
